a2afdd6e3902b00baff8853561c7be14661869ff
George Mcalpine <georgemcalpine@Georges-iMac.local>
2021-12-19 13:41:37 -0500

    fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,33 @@
 
-var playerName = window.prompt("What is your robot's name?", "Bob");
-console.log(playerName);
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+console.log(playerName, playerHealth, playerAttack);
 
-function fight(name) {
-    window.alert("The fight has begun! " + name);
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth -= playerAttack;
+    playerHealth -= enemyAttack;
+    console.log(enemyName + " attacked " + playerName + " and now has " + enemyHealth + " health");
+    console.log(playerName + " attacked " + enemyName + " and now has " + playerHealth + " health");
+
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!")
+    } 
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health");
+    }
+
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+      } 
+      else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+      }
 }
 
 fight(playerName);
